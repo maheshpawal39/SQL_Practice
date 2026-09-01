77d31c00ccf4c17001f9aee9d6f2c50ddbbdafe9
Mahesh Pawal <maheshpawal39@gmail.com>
2026-09-01 08:35:02 +0530

    Add files via upload

---
+++ 29-08-2026.sql
@@ -0,0 +1,95 @@
+-- 29-08-2026 --
+-- -- Q1 
+-- select * 
+-- from students ;
+
+-- -- Q2
+-- select name 
+-- from students;
+
+-- -- Q3
+-- select name , marks
+-- from students;
+
+-- -- Q4
+-- select city , age
+-- from students;
+
+-- -- Q5
+-- select studentID, name , course
+-- from students;
+
+-- -- Q6
+-- select * 
+-- from students
+-- where city = "indore";
+
+-- -- Q7
+-- select name , marks 
+-- from students 
+-- where city = "mumbai";
+
+-- -- Q8
+-- select *
+-- from students 
+-- where course = "BCA";
+
+-- -- Q9
+-- select name
+-- from students
+-- where course = "MCA";
+
+-- -- Q10
+-- select *
+-- from students
+-- where marks > 80;
+
+-- -- Q11
+-- select name , marks
+-- from students
+-- where marks < 70;
+
+-- -- Q12
+-- select * 
+-- from students
+-- where marks >= 85;
+
+-- -- Q13
+-- select *
+-- from students 
+-- where age > 22;
+
+-- -- Q14
+-- select name , age
+-- from students 
+-- where age <= 21;
+
+-- -- Q15
+-- select name , course , marks 
+-- from students
+-- where city = "delhi";
+
+-- -- Q16
+-- select name , course
+-- from students 
+-- where city = "Indore";
+
+-- -- Q17
+-- select *
+-- from students
+-- where marks = 95;
+
+-- -- Q18
+-- select name , city
+-- from students
+-- where age = 23;
+
+-- -- Q19
+-- select name , city , marks 
+-- from students 
+-- where marks > 75;
+
+-- -- Q20
+-- select *
+-- from students
+-- where city = "mumbai";
